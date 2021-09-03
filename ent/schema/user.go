707d6d5de3bedcd76442a3870166c71b4f244614
lemon-mint <ice.lemons.mint@gmail.com>
2021-09-04 08:50:02 +0900

    entgo.io

---
+++ ent/schema/user.go
@@ -0,0 +1,37 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+	"github.com/google/uuid"
+	"github.com/lemon-mint/open-backend/auth/password"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("uuid", uuid.New()).Unique(),
+		field.String("username").Unique(),
+		field.String("hash"),
+		field.String("salt"),
+		field.Int32("algorithm").Default(int32(password.DefaultAlgorithm)),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
+
+// Indexes of the User.
+func (User) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("username").Unique(),
+	}
+}
